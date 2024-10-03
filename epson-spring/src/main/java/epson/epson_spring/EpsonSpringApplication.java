5aff73c07393f81e9341efa73cd6b0a20b28dd85
yunrry <yunryy329@gmail.com>
2024-10-03 16:53:46 +0900

    spring-boot settint

---
+++ epson-spring/src/main/java/epson/epson_spring/EpsonSpringApplication.java
@@ -0,0 +1,13 @@
+package epson.epson_spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EpsonSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EpsonSpringApplication.class, args);
+	}
+
+}
---
+++ epson-spring/src/test/java/epson/epson_spring/EpsonSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package epson.epson_spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EpsonSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
